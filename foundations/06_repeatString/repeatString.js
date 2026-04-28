45c03b690d33e5d7adda1926cbb6168e14f23de6
Helium <130857426+Helium999@users.noreply.github.com>
2026-04-28 07:48:59 -0700

    Solve exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = ""
+    if(number < 0) {
+        return "ERROR"
+    }
+    else {
+        for(let i=0; i < number; i++) {
+        result = result.concat(string)
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
