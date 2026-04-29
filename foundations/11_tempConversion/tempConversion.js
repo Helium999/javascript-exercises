26904324f8ec455162be19c2a0f255854619082a
Helium <130857426+Helium999@users.noreply.github.com>
2026-04-28 21:58:05 -0700

    Solve exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  celsiusReading = ((tempInFahrenheit - 32) * 5/9)
+  return Number(celsiusReading.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  fahrenheitReading = ((tempInCelsius * 9/5)+32)
+  return Number(fahrenheitReading.toFixed(1))
 };
 
 // Do not edit below this line
