ddb3897d728444157e55768810f99fe26660f053
Helium <130857426+Helium999@users.noreply.github.com>
2026-04-28 21:08:19 -0700

    Solve exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >=0) {
+        numbers = []
+        const start = Math.min(a, b);
+        const end = Math.max(a, b);
+        for(let i = start; i <= end; i++) {
+            numbers.push(i)
+        }
+        sumOfNumbers = numbers.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+        }, 0);
+        return sumOfNumbers
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
