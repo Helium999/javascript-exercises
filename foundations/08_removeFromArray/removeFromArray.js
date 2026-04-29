7b81eb40753e23e6ccbe057cd073d1a990630ac0
Helium <130857426+Helium999@users.noreply.github.com>
2026-04-28 20:44:36 -0700

    Solve exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+//use rest parameters (...arguments) to include multiple
+//arguments in one array
+const removeFromArray = function(arr, ...otherArguments) {
+    filteredArray = arr.filter(n => !otherArguments.includes(n))
+    return filteredArray
 };
 
 // Do not edit below this line
