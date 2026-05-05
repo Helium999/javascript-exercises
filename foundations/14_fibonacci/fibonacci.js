4c8fd1c60e89e798f9808765b8a6b007726da0df
Helium <130857426+Helium999@users.noreply.github.com>
2026-05-04 22:13:54 -0700

    Solve exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index)
+    let sequence = [0, 1, 1]
+    
+    if(index < 0) {
+        return "OOPS"
+    }
+    else {
+        for(i = 0; i < index - 2; i++) {
+        const nextNum = sequence[sequence.length-2] + sequence[sequence.length-1]
+    sequence.push(nextNum)
+    }
+    return sequence[index]
+    }
 };
 
 // Do not edit below this line
