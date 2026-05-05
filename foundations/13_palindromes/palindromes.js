9beaeef1d5b18c6877e585b3d4bbe5dadf7fc266
Helium <130857426+Helium999@users.noreply.github.com>
2026-05-04 21:44:23 -0700

    Solve exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replaceAll(' ', '')
+                        .toLowerCase()
+                        .replace(/[\p{P}\p{S}]/gu, "") 
+    let reverseStr = cleanStr.split('')
+                                .reverse()
+                                .join('')
+    return cleanStr === reverseStr
 };
 
 // Do not edit below this line
