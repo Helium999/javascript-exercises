eb2b150b8dde871c23353409c20a0194a6fccd41
Helium <130857426+Helium999@users.noreply.github.com>
2026-05-04 22:44:13 -0700

    Solve exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear()
+    let sorted = arr.sort((a,b) => ((b.yearOfDeath ?? currentYear) - b.yearOfBirth) - ((a.yearOfDeath ?? currentYear) - a.yearOfBirth))
+    return sorted[0]
 };
 
 // Do not edit below this line
