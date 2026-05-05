9b42158c38fdfd95b6cbbeb8a551fe60e43fefa2
Helium <130857426+Helium999@users.noreply.github.com>
2026-05-04 21:06:00 -0700

    Solve exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total = arr.reduce(((acc, num) => acc + num), 0)
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multiplytotal = arr.reduce(((acc, num) => acc * num), 1)
+  return multiplytotal
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorialresult = 1
+  for(let i = 1 ; i <= number ; i++) {
+    factorialresult = factorialresult * i
+  }
+  return factorialresult
 };
 
 // Do not edit below this line
